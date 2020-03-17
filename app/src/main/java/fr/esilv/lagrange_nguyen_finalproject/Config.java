29d0965d4c6dae857d221fe062fda10c7dab1e16
justinelgr <jlagrange98@gmail.com>
2020-03-17 09:38:12 +0100

    commit

---
+++ app/src/main/java/fr/esilv/lagrange_nguyen_finalproject/Config.java
@@ -0,0 +1,10 @@
+package fr.esilv.lagrange_nguyen_finalproject;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyDfOc05bNcA4qaQksXE4Z7fH985hstjUZM";
+
+}
