7e4a64473f87cdad418e7343311680f1e7e96b32
caroline <caroline.1998@hotmail.fr>
2020-03-18 16:47:32 +0100

    Commit

--- app/src/main/java/fr/esilv/lagrange_nguyen_finalproject/Seasons.java
+++ app/src/main/java/fr/esilv/lagrange_nguyen_finalproject/Seasons.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.youtube.player.YouTubePlayer;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -52,17 +53,18 @@ public class Seasons extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.nav_season_1);
-        fragment.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.nav_season_1);
+        fragment.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
--- app/src/main/java/fr/esilv/lagrange_nguyen_finalproject/ui/Season1Fragment.java
+++ app/src/main/java/fr/esilv/lagrange_nguyen_finalproject/ui/Season1Fragment.java
@@ -1,6 +1,61 @@
 package fr.esilv.lagrange_nguyen_finalproject.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import fr.esilv.lagrange_nguyen_finalproject.Config;
+import fr.esilv.lagrange_nguyen_finalproject.R;
+
 public class Season1Fragment extends Fragment {
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_season1, container, false);
+        youTubeView = (YouTubePlayerView) root.findViewById(R.id.youtube_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, (YouTubePlayer.OnInitializedListener) getActivity());
+        return root;
+    }
+
+
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo("fhWaJi1Hsfo"); // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
+        }
+    }
+
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(getActivity(), RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), errorReason.toString());
+            Toast.makeText(getActivity(), error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY, (YouTubePlayer.OnInitializedListener) getActivity());
+        }
+    }
+
+    public YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return youTubeView;
+    }
+
 }
