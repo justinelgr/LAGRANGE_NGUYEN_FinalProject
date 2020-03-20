3633fb95a072ab28d17e8f9cbe24c6c22b124eb3
caroline <caroline.1998@hotmail.fr>
2020-03-20 01:30:49 +0100

    Commit

--- app/src/main/java/fr/esilv/lagrange_nguyen_finalproject/ui/Season5Fragment.java
+++ app/src/main/java/fr/esilv/lagrange_nguyen_finalproject/ui/Season5Fragment.java
@@ -1,6 +1,30 @@
 package fr.esilv.lagrange_nguyen_finalproject.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import fr.esilv.lagrange_nguyen_finalproject.R;
+import fr.esilv.lagrange_nguyen_finalproject.SeasonsTrailers;
+
 public class Season5Fragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_season5, container, false);
+        Button btn_trailer = (Button) root.findViewById(R.id.button_s5);
+        btn_trailer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Intent = new Intent(getActivity(), SeasonsTrailers.class);
+                startActivity(Intent);
+            }
+        });
+        return root;
+    }
 }
