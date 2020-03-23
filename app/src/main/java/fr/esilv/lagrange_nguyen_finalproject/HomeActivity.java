f9fc8f5394f32638698abd7613876bab37af451c
caroline <caroline.1998@hotmail.fr>
2020-03-24 00:39:45 +0100

    Commit

--- app/src/main/java/fr/esilv/lagrange_nguyen_finalproject/HomeActivity.java
+++ app/src/main/java/fr/esilv/lagrange_nguyen_finalproject/HomeActivity.java
@@ -13,6 +13,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 
     public void whatIsGG(View view){
