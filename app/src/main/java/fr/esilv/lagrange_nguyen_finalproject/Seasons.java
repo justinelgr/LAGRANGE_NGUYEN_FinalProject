fce6d70b9c92575c93e9acaf5ccc013bc90345b8
justinelgr <jlagrange98@gmail.com>
2020-03-24 16:45:11 +0100

    change of home screen

--- app/src/main/java/fr/esilv/lagrange_nguyen_finalproject/Seasons.java
+++ app/src/main/java/fr/esilv/lagrange_nguyen_finalproject/Seasons.java
@@ -2,23 +2,19 @@ package fr.esilv.lagrange_nguyen_finalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.youtube.player.YouTubePlayer;
-
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Seasons extends AppCompatActivity {
 
